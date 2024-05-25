5b07921da1332c1ed81850aad65f7ac0dd7e176d
yasharyas <yasharyas@proton.me>
2024-05-25 22:00:30 +0530

    upper navbar done for lg screen

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,27 @@
-import React,{useState,useEffect} from "react";
+import React, { useState } from "react";
+import Wrapper from "./Wrapper";
+import Link from "next/link";
+import Menu from "./Menu";
+
 
 const Header = () => {
-    const [mobileMenu, setMobileMenu]= useState(false);
-    const [showCatMenu,setShowCatMenu]= useState(false);
-    const [show,setShow]=useState("translate-y-0");
-    const [lastScrollY, setLastScrollY]= useState(0);
-    return <header className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}>Header</header>;
+    const [mobileMenu, setMobileMenu] = useState(false);
+    const [showCatMenu, setShowCatMenu] = useState(false);
+    const [show, setShow] = useState("translate-y-0");
+    const [lastScrollY, setLastScrollY] = useState(0);
+
+    return (
+        <header 
+            className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}
+        >
+            <Wrapper className="h-[60px] flex justify-between items-center">
+                <Link href="/">
+                <img src="/logo.svg" className="w-[40px md:w-[60px]"  />
+                </Link>
+                <Menu showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu}/>
+            </Wrapper>
+        </header>
+    );
 };
 
 export default Header;
