01b8e6cf8fa1b31dd00a38f52195023796705b40
yasharyas <yasharyas@proton.me>
2024-05-30 00:22:15 +0530

    hero banner completed

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
+import HeroBanner from "@/components/HeroBanner";
+
+
 export default function Home() {
-    return <main className="h-[2000px]">Home page....</main>;
+    return <main >
+        <HeroBanner
+            className=""
+        />
+    
+    </main>;
 }
