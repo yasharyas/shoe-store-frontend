2de8391672e618ff71b895e90ba673e34474d627
yasharyas <yasharyas@proton.me>
2024-05-30 11:58:27 +0530

    homepage front-end done

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -28,21 +28,21 @@ const HeroBanner = () => {
                     <img src="/slide-1.png" className="aspect-[16/10] md:aspect-auto object-cover" />
                     <div className="px-[15px] md:px-[40px] py-[10px] md:py-[25px]
                      font-oswald bg-white absolute bottom-[25px] md:bottom-[75px] left-0 text-black/[0.9] 
-                     text-[15px] md:text[30px] uppercase font-medium cursor-pointer hover:opacity-90"                     
+                     text-[30px] md:text[40px] uppercase font-medium cursor-pointer hover:opacity-90"                     
                      >Shop Now</div>
                 </div>
                 <div>
                     <img src="/slide-2.png" className="aspect-[16/10] md:aspect-auto object-cover" />
                     <div className="px-[15px] md:px-[40px] py-[10px] md:py-[25px]
                      font-oswald bg-white absolute bottom-[25px] md:bottom-[75px] left-0 text-black/[0.9] 
-                     text-[15px] md:text[30px] uppercase font-medium cursor-pointer hover:opacity-90"                     
+                     text-[30px] md:text[40px] uppercase font-medium cursor-pointer hover:opacity-90"                     
                      >Shop Now</div>
                 </div>
                 <div>
                     <img src="/slide-3.png" className="aspect-[16/10] md:aspect-auto object-cover" />
                     <div className="px-[15px] md:px-[40px] py-[10px] md:py-[25px]
                      font-oswald bg-white absolute bottom-[25px] md:bottom-[75px] left-0 text-black/[0.9] 
-                     text-[15px] md:text[30px] uppercase font-medium cursor-pointer hover:opacity-90"                     
+                     text-[30px] md:text[40px] uppercase font-medium cursor-pointer hover:opacity-90"                     
                      >Shop Now</div>
                 </div>
                 
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,37 @@
 import HeroBanner from "@/components/HeroBanner";
-
+import ProductCard from "@/components/ProductCard";
+import Wrapper from "@/components/Wrapper";
 
 export default function Home() {
     return <main >
-        <HeroBanner
-            className=""
-        />
+        <HeroBanner/>
+        <Wrapper>
+            {/* HEADING & PARAGRAPH START  */}
+            <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
+                <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">Shoe Amazing WoW</div>
+                <div className="text-md md:text-xl">Men sad men see shoe men heppy 
+                lorem ipsum dolor simet lorem ipsum dolor simet Men sad men see shoe men
+                 heppy lorem ipsum dolor simet lorem ipsum dolor simet</div>
+            </div>
+            {/* HEADING & PARAGRAPH END */}
+
+
+            {/* product grid start */}
+            <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
+                
+                <ProductCard/>
+                <ProductCard/>
+                <ProductCard/>
+                <ProductCard/>
+                <ProductCard/>
+                <ProductCard/>
+                <ProductCard/>
+                <ProductCard/>
+                <ProductCard/>
+            </div>
+            {/* product grid end  */}
+
+        </Wrapper>
     
     </main>;
 }
