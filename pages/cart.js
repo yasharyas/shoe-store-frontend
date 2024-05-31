eec2e11974a2e36146c7b8bb1a081057730d0a3b
yasharyas <yasharyas@proton.me>
2024-05-31 17:51:46 +0530

    fixed bugs (hamburger for navbar visible on desktop screen , Added Link to cart)

---
+++ pages/cart.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import Wrapper from "@/components/Wrapper";
+import CartItem from "@/components/CartItem";
+
+
+const Cart = () => {
+  return (
+    <div className="w-full md:py-20">
+        <Wrapper>
+            
+        </Wrapper>
+    </div>
+  )
+}
+
+export default Cart
