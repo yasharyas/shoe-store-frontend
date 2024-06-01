270e42f7bf860476816a955c01edfbfaba3c473d
yasharyas <yasharyas@proton.me>
2024-06-01 15:11:17 +0530

    completed summary section and empty cart page

--- pages/cart.js
+++ pages/cart.js
@@ -37,6 +37,25 @@ const Cart = () => {
 
             <div className='flex-[1]'>
               <div className='text-lg font-bold'>Summary</div>
+                <div className='p-5 my-5 bg-black/[0.5] rounded-xl'>
+                  <div className='flex justifu-between'>
+                    <div className='uppercase text-md md:text-lg font-medium text-black'>subtotal</div>
+                    <div className='text-md md:text-lg font-medium text-black'>Rs 40 230.00</div>
+                  </div>
+                  <div className='text-sm md:text-md py-5 border-t mt-5'>
+                  The subtotal reflects the total price of your
+                  order, including duties and taxes, before any
+                  applicable discounts. It does not include
+                  delivery costs and international transaction
+                </div>
+                </div>
+                {/* BUTTON START */}
+                <button
+                  className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 flex items-center gap-2 justify-center"
+                  >
+                  Checkout
+                </button>
+                 {/* BUTTON END */}
             </div>
 
             {/* SUMMARY END */}
@@ -44,6 +63,31 @@ const Cart = () => {
           </div>
 
           {/* CART CONTENT END */}
+
+          {/* this is empty screen  */}
+          {/* <div className="flex-[2] flex flex-col items-center pb-[50px] md:-mt-14">
+            <Image
+                src="/empty-cart.jpg"
+                width={300}
+                height={300}
+                className="w-[300px] md:w-[400px]"
+            />
+            <span className="text-xl font-bold">
+                Your cart is empty
+            </span>
+            <span className="text-center mt-4">
+                Looks like you have not added anything in your cart.
+                <br />
+                Go ahead and explore top categories.
+            </span>
+            <Link
+                href="/"
+                className="py-4 px-8 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 mt-8"
+            >
+                Continue Shopping
+            </Link>
+        </div> */}
+
         </Wrapper>
     </div>
   )
