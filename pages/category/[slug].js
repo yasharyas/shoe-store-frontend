95d9f076a45e4a59327613a41f259558f5dfe82a
yasharyas <yasharyas@proton.me>
2024-06-11 00:21:26 +0530

    pagination for category pages done

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -1,18 +1,35 @@
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import ProductCard from '@/components/ProductCard'
 import Wrapper from '@/components/Wrapper'
 import { fetchDataFromApi } from '@/utils/api'
+import useSWR from "swr";
 
+//pageination no. of items in one page
 
+const maxResult = 3;
 
 const Category = ({category,products,slug}) => {
+    const [pageIndex, setPageIndex] = useState(1);
+    const { query } = useRouter();
+
+    useEffect(() => {
+        setPageIndex(1);
+    }, [query]);
+
+    const { data, error, isLoading } = useSWR(
+        `/api/products?populate=*&[filters][categories][slug][$eq]=${slug}&pagination[page]=${pageIndex}&pagination[pageSize]=${maxResult}`,
+        fetchDataFromApi,
+        {
+            fallbackData: products,
+        }
+    );
     // const router = useRouter();
     // const { slug } =router.query;
 
   return (
-    <div className='w-full md:py-20'>
+    <div className='w-full md:py-20 relative'>
         <Wrapper>
             <div className='text-center max-w-[800px] mx-auto mt-8 md:mt-0'>
                 <div className='text-[28px] md:text-[34px] mb-[5] font-semibold leading-tight'>
@@ -23,7 +40,7 @@ const Category = ({category,products,slug}) => {
 
              {/* product grid start */}
              <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
-             {products?.data?.map((product) => (
+             {data?.data?.map((product) => (
                     <ProductCard key={product?.id} data={product} />
                 ))}
                 
@@ -38,6 +55,42 @@ const Category = ({category,products,slug}) => {
                 <ProductCard/> */}
             </div>
             {/* product grid end  */}
+
+            {/* PAGINATION BUTTONS START */}
+            {data?.meta?.pagination?.total > maxResult && (
+                <div className="flex gap-3 items-center justify-center my-16 md:my-0">
+                    <button
+                        className={`rounded py-2 px-4 bg-black text-white disabled:bg-gray-200 disabled:text-gray-500`}
+                        disabled={pageIndex === 1}
+                        onClick={() => setPageIndex(pageIndex - 1)}
+                    >
+                        Previous
+                    </button>
+
+                    <span className="font-bold">{`${pageIndex} of ${
+                        data && data.meta.pagination.pageCount
+                    }`}</span>
+
+                    <button
+                        className={`rounded py-2 px-4 bg-black text-white disabled:bg-gray-200 disabled:text-gray-500`}
+                        disabled={
+                            pageIndex ===
+                            (data && data.meta.pagination.pageCount)
+                        }
+                        onClick={() => setPageIndex(pageIndex + 1)}
+                    >
+                        Next
+                    </button>
+                </div>
+            )}
+            {/* PAGINATION BUTTONS END */}
+            {isLoading && (
+                <div className="absolute top-0 left-0 w-full h-full bg-white/[0.5] flex flex-col gap-5 justify-center items-center">
+                    <img src="/logo.svg" width={150} />
+                    <span className="text-2xl font-medium">Loading...</span>
+                </div>
+            )}
+
         </Wrapper>
     </div>
   )
@@ -65,8 +118,9 @@ export async function getStaticProps({ params: {slug} }) {
         `/api/categories?filters[slug][$eq]=${slug}`
     );
     const products = await fetchDataFromApi(
-        `/api/products?populate=*&[filters][categories][slug][$eq]=${slug}`
+        `/api/products?populate=*&[filters][categories][slug][$eq]=${slug}&pagination[page]=1&pagination[pageSize]=${maxResult}`
     );
+
 
     return {
         props: {
