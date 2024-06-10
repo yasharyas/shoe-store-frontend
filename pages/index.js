ffd433aafbd9b9b64b05ffac104d952834a52829
yasharyas <yasharyas@proton.me>
2024-06-10 16:54:22 +0530

    homepage products data fetching

--- pages/index.js
+++ pages/index.js
@@ -2,28 +2,12 @@ import HeroBanner from "@/components/HeroBanner";
 import ProductCard from "@/components/ProductCard";
 import Wrapper from "@/components/Wrapper";
 import { fetchDataFromApi } from "@/utils/api";
-import { useEffect , useState} from "react";
-
-
-
 
 export default function Home({products}) {
-    
-    // const [data, setData] = useState(null);
-    // useEffect(()=> {
-    //     fetchProducts();
-    // },[])
-
-    // const fetchProducts = async () => {
-    //     const {data} =await fetchDataFromApi('/api/products');
-    //     setData(data) ;
-    // }
-
-
 
     return <main>
         <HeroBanner/>
-        <h1>{products?.data?.[0]?.attributes?.name}</h1>
+        
         <Wrapper>
             {/* HEADING & PARAGRAPH START  */}
             <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
@@ -37,7 +21,10 @@ export default function Home({products}) {
 
             {/* product grid start */}
             <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
-                
+                {products?.data?.map((product) => (
+                    <ProductCard key={product?.id} data={product} />
+                ))}
+                {/* <ProductCard/>
                 <ProductCard/>
                 <ProductCard/>
                 <ProductCard/>
@@ -45,8 +32,7 @@ export default function Home({products}) {
                 <ProductCard/>
                 <ProductCard/>
                 <ProductCard/>
-                <ProductCard/>
-                <ProductCard/>
+                <ProductCard/> */}
             </div>
             {/* product grid end  */}
 
